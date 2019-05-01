433807e3dfea898c5b662db951cc6dbfda248ed7
846814595@qq.com <846814595@qq.com>
2019-05-01 13:23:04 +0800

    定时任务事务异常处理 配置单独的数据源

---
+++ src/main/java/com/jeesite/modules/businessjob/QuartzDataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.jeesite.modules.businessjob;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * springboot 默认整合quartz
+ * jdbc 持久化job 时 需要单配置数据源，
+ * 公用数据源会产生数据表被锁，事务异常回滚，导致job 信息无法从数据库中获取
+ * springboot2.0 + 官方文档说明
+ */
+@Configuration
+public class QuartzDataSourceConfig {
+
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl("jdbc:mysql://47.244.133.160:3306/jeesite?useSSL=false&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull");
+        druidDataSource.setUsername("root");
+        druidDataSource.setPassword("Root123@123");
+        return druidDataSource;
+    }
+}
