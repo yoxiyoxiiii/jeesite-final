6b0dd6f88828e9ad1566d2c3eb0f6e25b9aaf028
846814595@qq.com <846814595@qq.com>
2019-06-22 17:30:34 +0800

    调整包结构

--- src/main/java/com/jeesite/modules/basic/printer/entity/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.jeesite.modules.basic.printer.entity;
-
-import com.jeesite.common.entity.DataEntity;
-import com.jeesite.modules.basic.factory.entity.FactoryC;
-
-public class Printer extends DataEntity<FactoryC> {
-
-}
